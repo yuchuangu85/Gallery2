0f17bb9ea4defe63fa36d90ab587c69db02a92b1
MoXiang <yuchuangu85@gmail.com>
2021-03-17 15:14:19 +0800

    添加对应.h文件

--- app/src/main/jni/filters/filters.h
+++ app/src/main/jni/filters/filters.h
@@ -35,8 +35,6 @@ typedef unsigned int Color;
 
 #define LOG(msg...) __android_log_print(ANDROID_LOG_VERBOSE, "NativeFilters", msg)
 
-#define int interp(unsigned char *src, int p, int *off, float dr, float dg, float db)
-
 #define JNIFUNCF(cls, name, vars...) Java_com_android_gallery3d_filtershow_filters_ ## cls ## _ ## name(JNIEnv* env, jobject obj_unused __unused, vars)
 
 #define RED i
---
+++ app/src/main/jni/filters/fx.h
@@ -0,0 +1,10 @@
+//
+// Created by yuchuan on 2021/3/17.
+//
+
+#ifndef GALLERY_FX_H
+#define GALLERY_FX_H
+
+#define int interp(unsigned char *src, int p, int *off, float dr, float dg, float db)
+
+#endif //GALLERY_FX_H
